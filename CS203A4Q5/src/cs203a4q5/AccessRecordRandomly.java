0e247cb8738df3a381c9348dea65bcb246c1ea48
OriginalMidas <clayton.brez@gmail.com>
2016-11-26 11:25:27 -0600

    DisplayRecordSequentially complete and tested

---
+++ CS203A4Q5/src/cs203a4q5/AccessRecordRandomly.java
@@ -0,0 +1,29 @@
+/*
+ Author: Clayton Brezinski
+ UserID: 200220989
+ Date:
+ Notes:
+ */
+package cs203a4q5;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/*
+ Purpose:
+ Notes
+ */
+public class AccessRecordRandomly
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
+/*
+Purpose:
+Arguments:
+Outputs: 
+Notes:
+*/
