cbb001eb456f8bbec1282951b3b264c599e529d5
OriginalMidas <clayton.brez@gmail.com>
2016-11-26 18:51:32 -0600

    Create all files for Question 3

---
+++ CS203A4Q3/src/cs203a4q3/Artist.java
@@ -0,0 +1,28 @@
+/*
+Author: Clayton Brezinski
+UserID: 200220989
+Date: 
+Notes:
+*/
+
+package cs203a4q3;
+
+/*
+ *
+ * @author Dolan
+ */
+
+/*
+Purpose:
+Notes
+*/
+public class Artist
+{
+
+}
+/*
+Purpose:
+Arguments:
+Outputs: 
+Notes:
+*/
