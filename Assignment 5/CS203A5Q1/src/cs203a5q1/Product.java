c01dc99ed05988bbd5720ca81be1374d38ffa576
OriginalMidas <clayton.brez@gmail.com>
2016-12-07 15:38:24 -0600

    q1 complete and testing. Needs Comments

---
+++ Assignment 5/CS203A5Q1/src/cs203a5q1/Product.java
@@ -0,0 +1,64 @@
+/*
+Author: Clayton Brezinski
+UserID: 200220989
+Date: 
+Notes:
+*/
+
+package cs203a5q1;
+
+/*
+ *
+ * @author Dolan
+ */
+
+/*
+Purpose:
+Notes
+*/
+public class Product
+{
+    private String name;
+    private int ID;
+    private String description;
+
+    
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getID()
+    {
+        return ID;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setID(int ID)
+    {
+        this.ID = ID;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    
+
+    
+}
+/*
+Purpose:
+Arguments:
+Outputs: 
+Notes:
+*/
