7f233c227e16367e86fd9a06aceb23e3bbb9d751
OriginalMidas <clayton.brez@gmail.com>
2016-12-08 21:09:29 -0600

    print works as intended with all generics

--- Assignment 5/CS203A5Q3/src/cs203a5q3/CS203A5Q3.java
+++ Assignment 5/CS203A5Q3/src/cs203a5q3/CS203A5Q3.java
@@ -1,31 +1,47 @@
 /*
-Author: Clayton Brezinski
-Student ID: 200220989
-Notes:
-*/
-package cs203a5q3;
-import java.lang.Number;
-/**
- *
- * @author Dolan
+ Author: Clayton Brezinski
+ Student ID: 200220989
+ Notes:
  */
-public class CS203A5Q3 {
+package cs203a5q3;
+
+import java.lang.Number;
+
+/**
+
+ @author Dolan
+ */
+public class CS203A5Q3
+{
 
     /**
-     * @param args the command line arguments
+     @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // TODO code application logic here
-        int[] listA = {};
+        int[] arrayA =
+        {
+            9, 23, 6, 3, 83, 11, 1, 7, 5, 2
+        };
+        Integer[] listA = new Integer[arrayA.length];
+        arrayIntoList(listA, arrayA);
         System.out.println("Integer values in original order: ");
+        printArray(listA);
         System.out.println("Integer values in ascending order: ");
-        
-        double[] listB = {};
+
+        double[] arrayB =
+        {
+            9.9, 3.8, 5.2, 7.7, 1.5, 8.8, 3.3, 6.6, 8.3
+        };
+        Double[] listB = new Double[arrayB.length];
+        arrayIntoList(listB, arrayB);
         System.out.println("Double values in original order: ");
+        printArray(listB);
         System.out.println("Double values in ascending order: ");
     }
-    
-    public <T extends Number> void insertionSort(T[] list)
+
+    public static <T extends Number> void insertionSort(T[] list)
     {
         for (int i = 1; i < list.length; i++)
         {
@@ -35,7 +51,7 @@ public class CS203A5Q3 {
              */
             T currentElement = list[i];
             int k;
-            
+
             for (k = i - 1; k >= 0 && list[k].intValue() > currentElement.intValue(); k--)
             {
                 list[k + 1] = list[k];
@@ -45,12 +61,50 @@ public class CS203A5Q3 {
             list[k + 1] = currentElement;
         }
     }
-    public <T extends Number> void printArray(T[] list)
+
+    public static <T extends Number> void printArray(T[] list)
     {
-        for (int i = 1; i < list.length; i++)
+        if (Integer.class.isAssignableFrom(list[0].getClass()) == true)
         {
-            System.out.println(list[i].doubleValue());
+            for (int i = 1; i < list.length; i++)
+            {
+                System.out.println(list[i].intValue());
+            }
         }
+        else if (Double.class.isAssignableFrom(list[0].getClass()) == true)
+        {
+            for (int i = 1; i < list.length; i++)
+            {
+                System.out.println(list[i].doubleValue());
+            }
+        }
+        else
+        {
+            for (int i = 1; i < list.length; i++)
+            {
+                //System.out.println(list[i].doubleValue());
+                System.out.println("a");
+            }
+        }
+
+    }
+
+    public static <T extends Number> T[] arrayIntoList(T[] list, int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            list[i] = (T) Integer.valueOf(array[i]);
+        }
+        return list;
+    }
+
+    public static <T extends Number> T[] arrayIntoList(T[] list, double[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            list[i] = (T) Double.valueOf(array[i]);
+        }
+        return list;
     }
 }
 
