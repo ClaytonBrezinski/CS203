136b5be4b18bfb1f27de9f0be4398b871cc4bf00
OriginalMidas <clayton.brez@gmail.com>
2016-12-11 12:52:18 -0600

    Documented, added check to deposit amount

--- Assignment 5/CS203A5Q2/src/cs203a5q2/Account.java
+++ Assignment 5/CS203A5Q2/src/cs203a5q2/Account.java
@@ -6,28 +6,39 @@
  */
 package cs203a5q2;
 
-/*
-
- @author Dolan
- */
-
  /*
- Purpose:
+ Purpose: Holds an integer for balance and allows users to get the balance and deposit to the balance
  Notes
  */
 public class Account
 {
     private int balance = 0;
-
+    
+    /*
+    Purpose: Returns the balance
+    Arguments: N/A
+    Outputs: int - the balance of the current account
+    Notes:
+    */
     public int getBalance()
     {
         return balance;
     }
-
+    
+    /*
+    Purpose: Adds the given amount to the balance of the Account
+    Arguments: int - the amount we want to add to the balance
+    Outputs: n/a, deposit is incremented
+    Notes:
+    */
     public synchronized void deposit(int amount)
     {
+        if(amount < 1)
+        {
+            System.out.println("That is an invalid deposit amount");
+        }
         int newBalance = balance + amount;
-
+        
         // This delay is deliberately added to magnify the
         // data-corruption problem and make it easy to see.
         try
@@ -39,7 +50,7 @@ public class Account
             ex.printStackTrace();
         }
 
-        balance = newBalance;
+        this.balance = newBalance;
     }
 }
 /*
